b0b0135a1d6f9ecd444d79c9839ba148221b7a81
nilesh <nileshmaurya879@gmail.com>
2023-10-12 00:11:29 +0530

    updated package

--- UnitOfWorkRepositoryPatternProject.Infrastructure/Repository/Products/ProductRepository.cs
+++ UnitOfWorkRepositoryPatternProject.Infrastructure/Repository/Products/ProductRepository.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using UnitOfWorkRepositoryPatternProject.Core.Entities;
 using UnitOfWorkRepositoryPatternProject.Core.Interface;
-using UnitOfWorkRepositoryPatternProject.Core.Models;
 
 namespace UnitOfWorkRepositoryPatternProject.Infrastructure.Repository.Products
 {
